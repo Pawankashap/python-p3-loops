1d9b070c2baf13e1e1a218ecfb8f98203c19f845
Fnu Pawan Kumar <pawan.3940@gmail.com>
2023-07-05 15:33:57 -0700

    complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,37 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+      print(counter)
+      counter -= 1
+    print("Happy New Year!")
+    #pass
+
+# def fizzbuzzPrinter():
+#     for num in range(1, 101):
+#         print(fizzbuzz(num))
+
+
+def fizzbuzz():
+    for num in range(1, 101):
+      if num % 3 == 0 and num % 5 == 0:
+          print("FizzBuzz")
+      elif num % 3 == 0:
+          print("Fizz")
+      elif num % 5 == 0:
+          print("Buzz")
+      else:
+          print(num)
+    #pass
+
+def reverseString(str):
+  reversedStr = ""
+  for i in range(len(str)):
+    reversedStr = str[i] + reversedStr
+  return reversedStr
 
 def square_integers(int_list):
     # code goes here!
-    pass
-
-def fizzbuzz():
-    # code goes here!
-    pass
+    return list(map(lambda num: num ** 2, int_list))
+    #pass
